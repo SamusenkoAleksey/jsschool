ef79c252c034e21f461cd7cc708528a53568068b
SamusenkoAleksey <aleksej.samusenko93@gmail.com>
2016-07-01 23:13:20 +0300

    slider task is done

--- js (technical tasks)/_block/Slider.js
+++ js (technical tasks)/_block/Slider.js
@@ -3,69 +3,68 @@ function Slider(div) {
 		return;
 	}
 	this.div = div;
+	this.widthOfDiv = this.div.clientWidth;
 	this.createDataSlide();
-	this.createLinks();
 	this.clickEvent();
+	this.setActiveElement(this.controlsDiv.children[0]);
 }
 
 Slider.prototype = Object.create(App.prototype);
 Slider.prototype.constructor = Slider;
 
+
+
+Slider.prototype.listOfSlides = document.querySelector('.val-list-slider');
 Slider.prototype.controlsDiv = document.querySelector('.val-display-controls');
 
-Slider.prototype.getListOfSlides = function () {
-	return document.querySelector('.val-list-slider').children;
-}
-
 Slider.prototype.createDataSlide = function () {
-	var num = 1;
+	var value = 1,
+		stringDataControls = '';
 		
-	for (var i = 0; i < this.getListOfSlides().length ; i++){
-		this.getListOfSlides()[i].setAttribute('data-slide', num);
-		num ++;
+	for (var i = 0; i < this.listOfSlides.children.length ; i++){
+
+		this.listOfSlides.children[i].setAttribute('data-slide', value);
+
+		stringDataControls += '<span data-controls=\'' + value + '\'></span>';
+		value ++;
 	};
 
+	this.controlsDiv.insertAdjacentHTML('afterBegin', stringDataControls);
 }
 
 
-Slider.prototype.createLinks = function () {
-		str = '',
-		num = 1;
-		
-	for(var i = 0; i < this.getListOfSlides().length ; i++ ){
-		str += '<span data-controls=\'' + num + '\'></span>';
-		num ++; 
-	};
-
-	this.controlsDiv.insertAdjacentHTML('afterBegin', str); 
-
+Slider.prototype.setActiveElement = function (element) {
+	var otherActiveElement = document.querySelector('.-active-slide');
+	
+	if(element.classList.contains('-active-slide')){
+		return;
+	}else if (otherActiveElement){
+		otherActiveElement.classList.remove('-active-slide');
+	}
+	
+	element.classList.add('-active-slide');
 }
+
+
+Slider.prototype.moveSlider = function (value) {
+
+	this.listOfSlides.style.cssText = 'transform: translateX(' + '-' + this.widthOfDiv * (value - 1) + 'px); transition:' + 1.5 +'s;';
+}
+
 
 Slider.prototype.clickEvent = function () {
-	this.controlsDiv.addEventListener('click', goToSlide);
-
 	var that = this;
+	that.controlsDiv.addEventListener('click', goToSlide);
+
+	
 	function goToSlide (e) {
 		var target = e && e.target || e.srcElement;
 		if(target.hasAttribute('data-controls')){
-			//console.log(target);
-			that.addActiveStatus(target);
-		}
+            that.setActiveElement(target);
+			that.moveSlider(target.getAttribute('data-controls'));
+		};
 	}
 }
-
-
-Slider.prototype.addActiveStatus = function (element) {
-	if(element.classList.contains('-active-slide')){
-		return;
-	}else{
-		var activeElement = document.querySelector('.-active-slide');
-			activeElement.classList.remove('-active-slide');
-
-			element.classList.add('-active-slide');
-	}
-}
-
 
 new Slider (document.querySelector('.val-slider-general-news'));
 
