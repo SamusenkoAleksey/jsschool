c5ff1d5da83c16f535986eb6e7f4aff7c7c6e611
SamusenkoAleksey <aleksej.samusenko93@gmail.com>
2016-07-10 12:51:43 +0300

    Category_Single task is done

---
+++ js_(technical tasks)/_block/Category_Single.js
@@ -0,0 +1,70 @@
+function CategorySingle () {
+    this.inputElement = document.getElementById('val-count-and-id');
+    this.dataId = this.inputElement.getAttribute('data-id');
+    this.dataOffset = this.inputElement.getAttribute('data-count');
+    this.newsLoader();
+    this.statusLoad = false;
+}
+
+CategorySingle.prototype = Object.create(App.prototype);
+CategorySingle.prototype.constructor = CategorySingle;
+
+
+CategorySingle.prototype.newsLoader = function () {
+    var that = this;
+    window.addEventListener('load', startLoading);
+
+    function startLoading () {
+        that.Ajax('http://user110.js.uitclassroom.com/site/GetCategoryByIdXhrOrNotId?id=' + that.dataId + '&offset=' + that.dataOffset, that.getValue, that);
+    }
+
+    window.addEventListener('scroll', continuteLoading);
+
+    function continuteLoading () {
+
+        var totalPageHeight = document.body.clientHeight,
+            tippingPoint = totalPageHeight - 100,
+            currentScrollingY = window.scrollY + window.innerHeight;
+
+        if(currentScrollingY > tippingPoint  && that.statusLoad === false) {
+            that.Ajax('http://user110.js.uitclassroom.com/site/GetCategoryByIdXhrOrNotId?id=' + that.dataId + '&offset=' + that.dataOffset, that.getValue, that);
+
+        }
+    }
+}
+
+CategorySingle.prototype.getValue = function (value, self) {
+    if(value === null || value === undefined){
+        return;
+    }
+    var categoryDiv = document.getElementById('val-single-category'),
+        language = value.language,
+        offset = value.offset,
+        news = JSON.parse(value.news);
+
+    self.dataOffset = offset;
+    self.statusLoad = true;
+    categoryDiv.insertAdjacentHTML('beforeEnd', self.createHtmlElement(language, news), self);
+    self.statusLoad = false;
+}
+
+CategorySingle.prototype.createHtmlElement = function (lang, arr, self) {
+    var string = '';
+
+    for(var i = 0 ; i < arr.length ; i++ ){
+        string += '<a href="/site/news/' + arr[i].id + '" class="val-block-gen-news">'
+                    + '<div class="val-image-block-gen-news">'
+                        + '<img src="/uploads/news/thumb/' + arr[i].image + '">'
+                    + '</div>'
+                    + '<div class="val-description-block-gen-news">'
+                        + '<span class="val-news-view">' + arr[i].views + '</span>'
+                        + '<span class="val-content-news-data">' + arr[i].date + '</span>'
+                        + '<h3 class="val-content-news-title-small">' + arr[i]['title_' + lang] + '</h3>'
+                    + '</div>'
+                + '</a>';
+    }
+
+    return string;
+}
+ß
+new CategorySingle();
